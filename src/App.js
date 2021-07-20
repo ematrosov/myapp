012da15661b70a2ff03d9513d9591bac2af742cb
matrosov <egor.matrosov@gmail.com>
2021-07-20 19:13:30 +0700

    HW3

--- src/App.js
+++ src/App.js
@@ -1,91 +1,31 @@
-import './App.css';
-import { useCallback, useEffect, useRef, useState } from "react";
+import Chat from './components/chat'
+import ChatsList from "./components/chats-list";
+import { Box, createTheme, makeStyles, MuiThemeProvider } from "@material-ui/core";
 
-
-
-function Message(props) {
-
-  return <div className={props.fromCurrentUser ? 'message message__right' : 'message message__left'}>
-    <p className="message_author">{props.fromCurrentUser ? 'Автор' : props.author}</p>
-    <p className="message_item">{props.text}</p>
-
-  </div>
-
-}
+const useStyles = makeStyles({
+  App: {
+    textAlign: 'center',
+    display: "flex",
+    flexDirection: 'row'
+  }
+})
 function App() {
-  const [messageList, setMessageList] = useState([]);
-  const [id, setId] = useState(1)
-  const [currentUser] = useState('Matrosov Egor')
-  const [currentMessage, setCurrentMessage] = useState('')
-  const messageContainerRef = useRef(null);
-
-  const addMessage = useCallback((text, author) => {
-    const msg = {
-      id: id,
-      author: author,
-      text: text
+  const classes = useStyles();
+  const darkTheme = createTheme({
+    palette: {
+      type: 'light'
     }
-
-    setMessageList((prevMessageList) => {
-      return prevMessageList.concat(msg);
-    });
-
-    setId((oldId) => oldId + 1);
-  }, [id]);
-
-  const handleInputChange = (event) => {
-    setCurrentMessage(event.target.value)
-  }
-
-  const handleSubmitForm = (event) => {
-    event.preventDefault();
-    addMessage(currentMessage, currentUser)
-    setCurrentMessage('')
-  }
-
-  const robotAnswer = () => {
-    if (messageList.length > 0 && messageList[messageList.length - 1].author !== 'Bot') {
-      const robotMessage = 'Заполните еще раз'
-      setTimeout(() => addMessage(robotMessage, 'Bot'), 1500)
-    }
-  }
-
-  useEffect(robotAnswer, [messageList, addMessage])
-
-  useEffect(() => {
-    messageContainerRef.current.scrollTop = messageContainerRef.current.scrollHeight;
-  }, [messageList])
-
+  })
   return (
-    <div className="App">
+    <MuiThemeProvider theme={darkTheme} >
+      <Box className={classes.App}>
+        <ChatsList />
+        <Chat />
+      </Box>
+    </MuiThemeProvider >
+  )
 
-      <div className="message_container" ref={messageContainerRef}>
-        {
-          messageList.map((message) =>
-            <Message key={message.id}
-              text={message.text}
-              author={message.author}
-              fromCurrentUser={message.author === currentUser} />)
-        }
-      </div>
-      <form onSubmit={handleSubmitForm} className="form_container">
-        <textarea value={currentMessage} onChange={handleInputChange} className="form_input" />
-        <input type="submit" value="Отправить" className="form_button" />
-      </form>
-    </div>
-  );
 }
-
-// function App(props) {
-//  return (
-//    <div className="App">
-//     <header className="App-header">
-//       My First React App
-//       <h3>Hello, {props.name}</h3>
-//    </header>
-//   </div>
-// );//
-
 export default App;
 
 
---
+++ src/components/chat.js
@@ -0,0 +1,110 @@
+import Message from "./message";
+import { useCallback, useEffect, useRef, useState } from "react";
+import { Box, FormControl, Grid, IconButton, makeStyles, Paper, Typography } from "@material-ui/core";
+import TextField from "@material-ui/core/TextField";
+import SendIcon from "@material-ui/icons/Send";
+
+const useStyles = makeStyles({
+    root: {
+        minWidth: "70%",
+        padding: "0 20px",
+        boxSizing: "border-box"
+    },
+    chatContainer: {
+        height: "80vh",
+        overflowY: "scroll",
+        display: "flex",
+        flexDirection: "column",
+        marginBottom: 20
+    },
+    input: {
+        minWidth: "80%"
+    }
+})
+
+export default function Chat() {
+    const classes = useStyles();
+    const [messageList, setMessageList] = useState([]);
+    const [id, setId] = useState(1)
+    const [currentUser] = useState('Matrosov Egor')
+    const messageContainerRef = useRef(null);
+    const inputRef = useRef(null);
+    const [currentMessage, setCurrentMessage] = useState('')
+
+    const addMessage = useCallback((text, author) => {
+        const msg = {
+            id: id,
+            author: author,
+            text: text
+        }
+
+        setMessageList((prevMessageList) => {
+            return prevMessageList.concat(msg);
+        });
+
+        setId((oldId) => oldId + 1);
+    }, [id]);
+
+
+    const robotAnswer = () => {
+        if (messageList.length > 0 && messageList[messageList.length - 1].author !== 'Bot') {
+            const robotMessage = 'Еще раз...'
+            setTimeout(() => addMessage(robotMessage, 'Bot'), 1500)
+        }
+    }
+
+    const handleInputChange = (event) => {
+        setCurrentMessage(event.target.value)
+    }
+
+    const handleSubmitForm = (event) => {
+        event.preventDefault();
+        addMessage(currentMessage, currentUser)
+        setCurrentMessage('')
+    }
+
+    useEffect(robotAnswer, [messageList, addMessage])
+
+    useEffect(() => {
+        messageContainerRef.current.scrollTop = messageContainerRef.current.scrollHeight;
+        inputRef.current.focus()
+    }, [messageList])
+
+    return (
+        <Paper className={classes.root}>
+            <Typography variant="h5" >
+                Чат
+            </Typography>
+            <Box className={classes.chatContainer} ref={messageContainerRef}>
+                <Grid container direction="column" >
+                    {
+                        messageList.map((message) =>
+                            <Message key={message.id}
+                                text={message.text}
+                                author={message.author}
+                                fromCurrentUser={message.author === currentUser} />)
+                    }
+                </Grid>
+            </Box>
+
+            <form onSubmit={handleSubmitForm} >
+                <FormControl className={classes.input}>
+                    <TextField label="Сообщение"
+                        id='text-field'
+                        multiline
+                        rows={3}
+                        variant="outlined"
+                        value={currentMessage}
+                        onChange={handleInputChange}
+                        autoFocus
+                        required
+                        inputRef={inputRef}
+                    />
+                </FormControl>
+                <IconButton area-label='send' type={"submit"}>
+                    <SendIcon color="primary" />
+                </IconButton>
+            </form>
+        </Paper>
+    );
+}
