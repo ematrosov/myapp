1f0e3a5e7938706a26d5be82e75211417daa31a0
matrosov <egor.matrosov@gmail.com>
2021-08-12 17:45:00 +0700

    HW9

--- src/redux/messages/messages-reducer.js
+++ src/redux/messages/messages-reducer.js
@@ -1,21 +1,22 @@
 import { ADD_MESSAGE } from "./messages-actions";
 
 const initialState = [
-    { id: 1, chatId: 1, currentUser: true, text: '11' },
-    { id: 2, chatId: 1, currentUser: false, text: '112' },
-    { id: 3, chatId: 2, currentUser: true, text: '333' },
-    { id: 4, chatId: 2, currentUser: false, text: '4444' },
-    { id: 5, chatId: 3, currentUser: true, text: '5555' },
-    { id: 6, chatId: 3, currentUser: false, text: '66666' }
+    //{ id: 1, chatId: 1, currentUser: true, text: '11' },
+    // { id: 2, chatId: 1, currentUser: false, text: '112' },
+    // { id: 3, chatId: 2, currentUser: true, text: '333' },
+    // { id: 4, chatId: 2, currentUser: false, text: '4444' },
+    // { id: 5, chatId: 3, currentUser: true, text: '5555' },
+    // { id: 6, chatId: 3, currentUser: false, text: '66666' }
 ]
 
 export default function messagesReducer(state = initialState, action) {
     switch (action.type) {
         case ADD_MESSAGE: {
-            return [
-                ...state,
-                action.payload.message
-            ]
+            const index = state.findIndex(msg => msg.id === action.payload.message.id)
+            if (index === -1) {
+                return [...state, action.payload.message]
+            }
+            return [...state]
         }
         default:
             return state
